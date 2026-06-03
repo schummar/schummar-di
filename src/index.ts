05ed3fb7b4d1f709b65f685c77b88c6116aaf5e8
Marco Schumacher <marco@schumacher.dev>
2026-06-03 14:13:53 +0200

    fix: added missing exports

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
-export { BackgroundService } from './backgroundService';
+export { BackgroundService, BackgroundServiceWithContainer } from './backgroundService';
 export { createContainer } from './container';
-export { Injectable } from './injectable';
+export { Injectable, InjectableWithContainer } from './injectable';
 export { DisposeError, InjectionError, StartError } from './lib/errors';
 export * from './serviceDescriptionHelpers';
 export type {
